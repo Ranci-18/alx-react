6c77d997ee4757efc65709c52d78fd405affa59d
Frank <wanyoike39@gmail.com>
2023-10-28 20:13:34 +0300

    project from scratch

---
+++ 0x01-react_intro/task_5/dashboard/Notifications/Notifications.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils/utils';
+import closeIcon from '../assets/close-icon.png';
+
+function Notifications() {
+    const inLineStyle = {
+        position: 'absolute',
+        right: '1px',
+        top: '1px',
+    }
+    const buttonOnClick = () => {
+        console.log('Close button has been clicked');
+    }
+    return (
+        <div className="Notifications">
+            <button style={inLineStyle} onClick={buttonOnClick} aria-label='close'>
+                <img src={closeIcon} alt='close-icon' width="10px" />
+            </button>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data="default">New course available</li>
+                <li data="urgent">New resume available</li>
+                <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
+            </ul>
+        </div>
+    )
+}
+
+export default Notifications;
--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,46 +1,45 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '../dist')
     },
-    mode: "development",
-    devServer: {
-        static: "../dist",
-        hot: true,
-        compress: true,
-        open: true,
-        port: 8564,
-        },
-    devtool: "inline-source-map",
-    resolve: {
-        extensions: [".js", ".jsx"],
-        },
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-                },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-                },
-            {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ["babel-loader"],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ],
+                    },
                 },
-            ],
-        },
-    plugins: [
-        new HtmlWebpackPlugin({
-            name: "index.html",
-            inject: false,
-            template: "./dist/index.html",
-            }),
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                use: [
+                    'file-loader',
+                ],
+            }
         ],
+    },
+    devServer: {
+        static: path.resolve(__dirname, '../dist'),
+        hot: true,
+        open: true,
+    }
 }
