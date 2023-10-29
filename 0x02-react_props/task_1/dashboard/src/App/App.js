1efeb1b041af7d6cc241440cc63a1046eba30322
Frank <wanyoike39@gmail.com>
2023-10-29 19:24:39 +0300

    react props project

---
+++ 0x02-react_props/task_1/dashboard/src/App/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import React from 'react';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Notifications />
+      <div className="App">      
+        <Header />
+        <div className="App-body">
+          <Login />
+        </div>
+        <div className="App-footer">
+          <Footer />
+        </div>
+      </div>
+    </React.Fragment>  
+  );
+}
+
+export default App;
