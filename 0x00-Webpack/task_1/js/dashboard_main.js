c6c0cd8d12d07e3d201cd90d1c686ddf57719e38
Frank <wanyoike39@gmail.com>
2023-10-29 23:50:31 +0300

    webpack starter project

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -5,12 +5,12 @@ $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$('body').append('<p>Copyright - Holberton School');
 
 let count = 0;
-const updateCounter = () => {
+function updateCounter() {
     count++;
-    $('#count').html(`${count} clicks on the button`);
-};
+    $('#count').text(`${count} clicks on the button`);
+}
 
-$('button').on('click', _.debounce(updateCounter, 300));
+$('button').on('click', _.debounce(updateCounter, 350));
