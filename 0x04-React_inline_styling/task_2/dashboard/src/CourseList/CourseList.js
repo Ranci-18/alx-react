29db73bbf2d39e357fc3e96a33c6ea6eb0fd5e18
Frank <wanyoike39@gmail.com>
2023-11-07 06:17:29 +0300

    inline stying

---
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import CourseListRow from './CourseListRow';
+import './CourseList.css';
+import CourseShape from './CourseShape';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  
+});
+export default function CourseList({ listCourses }) {
+  return (
+    <table id='CourseList'>
+      <thead>
+        <CourseListRow
+        isHeader={true}
+        textFirstCell='Available courses' />
+
+        <CourseListRow
+        textFirstCell='Course name'
+        textSecondCell='Credit'
+        isHeader={true} />
+      </thead>
+      <tbody>
+        {listCourses.length > 0 ? (
+          listCourses.map(({ id, name, credit}) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
+        ) : (
+          <CourseListRow textFirstCell='No course available yet' />
+        )}
+      </tbody>
+    </table>
+  )
+}
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
+};
+
+CourseList.defaultProps = {
+  listCourses: [],
+};
