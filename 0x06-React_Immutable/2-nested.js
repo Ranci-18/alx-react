dd0f72e094536a1d98f77c6f1f3dd4ff61703fe5
Frank <wanyoike39@gmail.com>
2023-11-16 10:59:57 +0300

    react immutable

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,14 +1,6 @@
-const { Map } = require('immutable');
+import { Map } from 'immutable';
 
 function accessImmutableObject(object, array) {
     const object1 = Map(object);
     return object1.getIn(array);
 }
-
-console.log(accessImmutableObject({
-    name: {
-         first: "Guillaume",
-         last: "Salva"
-    }
-}, ['name', 'first'])
-);
