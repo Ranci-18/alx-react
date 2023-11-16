fb6985dd0d5d90e17080da2a41b88d1b734187d6
Frank <wanyoike39@gmail.com>
2023-11-16 16:14:36 +0300

    react immutable

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,18 +1,18 @@
-import { Seq } from 'immutable';
+import { Seq } from 'immutable'
 
-function capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+function capitalizeFirstLetter (str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
-export default function printBestStudents(object) {
-    const seqData = Seq(object);
-    const bestStudents = seqData
-        .filter((student) => student.score > 70)
-        .map((student) => {
-            return {
-                score: student.score,
-                firstName: capitalizeFirstLetter(student.firstName),
-                lastName: capitalizeFirstLetter(student.lastName),
-            }
-        });
-    console.log(bestStudents.toJS());
+export default function printBestStudents (object) {
+  const seqData = Seq(object)
+  const bestStudents = seqData
+    .filter((student) => student.score > 70)
+    .map((student) => {
+      return {
+        score: student.score,
+        firstName: capitalizeFirstLetter(student.firstName),
+        lastName: capitalizeFirstLetter(student.lastName)
+      }
+    })
+  console.log(bestStudents.toJS())
 }
