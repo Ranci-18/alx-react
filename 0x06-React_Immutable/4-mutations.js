151453349bbbdde4438ea3703aac2fc06d279ece
Frank <wanyoike39@gmail.com>
2023-11-16 11:35:36 +0300

    react immutable

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -10,5 +10,5 @@ export const map = Map({
 });
 
 export const map2 = map.withMutations((mutableMap) => {
-    mutableMap.set('2', 'Benjamin').set('4', 'Oliver');
+    mutableMap.set(2, 'Benjamin').set(4, 'Oliver');
 });
